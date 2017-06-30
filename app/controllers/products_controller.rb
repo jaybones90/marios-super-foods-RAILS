60bc028bb320ff9fda0b5a000a69f162019ac104
Jason Ainsworth <jasoncruze@gmail.com>
2017-06-30 12:09:05 -0700

    add in full CRUD for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,29 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+
+private
+  def product_params
+    params.require(:product).permit(:name, :country, :cost)
+  end
 
 end
